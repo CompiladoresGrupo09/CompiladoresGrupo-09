67db561dfe746003ea864990eb4d70f71b7904ca
Brwnds <Brwnds@users.noreply.github.com>
2026-09-06 14:12:36 +0000

    deploy: 6ebe3b719c148498e118c4c9fb77129d5a37c592

---
+++ tests/validos/tokens_basico.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// Arquivo de teste: cobre palavras reservadas, identificadores, literais e operadores do nucleo
+int main() {
+    int quantidade = 10;
+    float preco = 2.5;
+    char letra = 'a';
+
+    if (quantidade > 0 && preco <= 100) {
+        quantidade = quantidade - 1;
+    } else {
+        quantidade = 0;
+    }
+
+    while (quantidade != 0) {
+        quantidade = quantidade - 1;
+    }
+
+    for (quantidade = 0; quantidade < 5; quantidade = quantidade + 1) {
+        printf("%d", quantidade);
+    }
+
+    return 0;
+}
